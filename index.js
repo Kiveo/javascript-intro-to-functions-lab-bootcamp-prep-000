c3462399c34678a081fba90ff569799a5ffa241d
Maverick Tesini <mavericktesini@gmail.com>
2017-10-17 21:42:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function sayHiToGrandma(string) {
   // mixedCase.toUpperCase() === mixedCase
   // Check ---if--- string is lower, upper, or 'I love you, Grandma'
   if ( lowercase.toLowerCase() === lowercase) {
-    return "I can\'t hear you!";
+    return 'I can\'t hear you!';
   }
   else if (uppercase.toUpperCase() === uppercase) {
-    return "YES INDEED!";
+    return 'YES INDEED!';
   }
   else if (string == "I love you, Grandma.") {
     return "I love you, too.";
